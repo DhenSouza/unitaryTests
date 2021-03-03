6f81d47474865eced6648504447ecdfa04be6525
ShenSouza <ds052798ts@gmail.com>
2021-03-03 10:35:50 -0300

    chore(projeto): Iniciando projeto de teste #1

---
+++ unitaryTestOne/src/main/java/com/generation/unitarytest/UnitaryTestOneApplication.java
@@ -0,0 +1,13 @@
+package com.generation.unitarytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitaryTestOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitaryTestOneApplication.class, args);
+	}
+
+}
---
+++ unitaryTestOne/src/test/java/com/generation/unitarytest/UnitaryTestOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.unitarytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitaryTestOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
